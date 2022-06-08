0a47113a8a0c3920111500397059fbf11e9a9d09
marco95OP <41342819+marco95OP@users.noreply.github.com>
2022-06-08 16:19:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu(event) {
+  var navBar = document.getElementById("mainNav");
+  var expanded = event.currentTarget.getAttribute("aria-expanded");
+  if (expanded==="true") {
+    navBar.classList.add("closed");
+    navBar.classList.remove("opened");
+    event.currentTarget.setAttribute('aria-expanded', 'false');
+  } else {
+    navBar.classList.add("opened");
+    navBar.classList.remove("closed");
+    event.currentTarget.setAttribute('aria-expanded', 'true');
+  }
+
+}
+
+document.getElementById('menuButton').addEventListener('click', toggleMenu, false);
